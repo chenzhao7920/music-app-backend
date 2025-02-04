742068191e362d57ce4615909ee66a00205f0534
chenzhao7920 <chenzhao7920@gmail.com>
2025-02-04 17:16:41 -0500

    add postgresql db

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,5 @@ module DiscogsBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    # Add this line to disable database for now
-    config.middleware.delete ActiveRecord::Migration::CheckPending
   end
 end
